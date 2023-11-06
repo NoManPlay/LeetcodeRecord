3f1a7441611e5a1b222400908fb4df09486b5923
qintianxing <qintianxing@100.me>
2023-11-06 10:21:02 +0800

    代码随想录_双指针法

--- 27.移除元素.go
+++ 27.移除元素.go
@@ -5,24 +5,40 @@
  */
 
 // @lc code=start
+
+//双向指针
+// func removeElement(nums []int, val int) int {
+// 	left := 0
+// 	right := len(nums) - 1
+// 	for left <= right {
+// 		for left <= right && nums[left] != val {
+// 			left++
+// 		}
+// 		for left <= right && nums[right] == val {
+// 			right--
+// 		}
+// 		if left < right {
+// 			nums[left] = nums[right]
+// 			left++
+// 			right--
+// 		}
+// 	}
+// 	fmt.Println(nums)
+// 	return left
+// }
+
+//快慢指针
 func removeElement(nums []int, val int) int {
-	left := 0
-	right := len(nums) - 1
-	for left <= right {
-		for left <= right && nums[left] != val {
-			left++
-		}
-		for left <= right && nums[right] == val {
-			right--
-		}
-		if left < right {
-			nums[left] = nums[right]
-			left++
-			right--
+	length:=len(nums)
+	res:=0
+	for i := 0; i < length; i++ {
+		if nums[i]!=val{
+			nums[res]=nums[i]
+			res++
 		}
 	}
-	fmt.Println(nums)
-	return left
+	nums=nums[:res]
+	return res
 }
 
 // @lc code=end
